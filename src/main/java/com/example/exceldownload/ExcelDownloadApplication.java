d44d16011b5c5f0774a29e4f9308c53dba5f12e9
jkkim <feelink_jgkim@woowahan.com>
2018-06-02 12:58:50 +0900

    spring boot admin 설정.
    https://m.blog.naver.com/PostView.nhn?blogId=userspace&logNo=220827444046&proxyReferer=https%3A%2F%2Fwww.google.co.kr%2F

--- src/main/java/com/example/exceldownload/ExcelDownloadApplication.java
+++ src/main/java/com/example/exceldownload/ExcelDownloadApplication.java
@@ -1,9 +1,16 @@
 package com.example.exceldownload;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableScheduling
 public class ExcelDownloadApplication {
 
 	public static void main(String[] args) {
